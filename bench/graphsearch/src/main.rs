c3d99f8682685a1a6ea91c62ff3495bd85a3b02b
Yash Kumar <kumaryash6352@gmail.com>
2025-10-05 18:11:34 -0500

    fix reporting

--- bench/graphsearch/src/main.rs
+++ bench/graphsearch/src/main.rs
@@ -92,10 +92,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     eprintln!("[PE {:>2}] parsed {} searchpairs", mype, all_searches.len());
     
     // don't need the shvec overhead anymore
-    let local_searches = all_searches.iter().collect::<Vec<_>>();
+    let searches = all_searches.iter().collect::<Vec<_>>();
     let mut distances = Vec::with_capacity(local_searches.len());
     eprintln!("[PE {:>2}] starting searches!", mype);
-    for (from, to) in local_searches {
+    for (from, to) in searches.iter().skip(mype * slines_per_pe).take(slines_per_pe) {
         eprintln!("[PE {mype:>2}]search #{:>4}: {from:>10} -> {to:>10}...", distances.len());
         distances.push(bfs(*from, *to, &adj, &ctx, &shm));
     }
